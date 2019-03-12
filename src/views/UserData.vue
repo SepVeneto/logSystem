1836206c07a0ce0eafd6170bb4225862d72f3188
qienze <qienze@beyondcent.com>
2019-03-12 22:24:15 +0800

    add  to pagination

--- src/views/UserData.vue
+++ src/views/UserData.vue
@@ -6,8 +6,10 @@
             <th class="tag">名称</th> 
             <th class="tag">住址</th> 
             <th class="tag">手机号</th>
+            <th class="tag">操作</th>
         </tr>
         <tr 
+        class="table-tr"
         v-for="item in pageData" 
         :key="item.id"
         >
@@ -15,6 +17,10 @@
             <td class="tag-content">{{item.name}}</td> 
             <td class="tag-content">{{item.address}}</td> 
             <td class="tag-content">{{item.phone}}</td>
+            <td class="tag-content">
+                <button class="table-button" id="edit">编辑</button>
+                <button class="table-button" id="delete">删除</button>
+            </td>
         </tr>
     </table>
     <pagination></pagination>
@@ -36,7 +42,7 @@ export default {
     },
     mounted() {
         let m = Mock.mock({
-            'data|10-30': [{
+            'data|20': [{
                 'id|+1': 1,
                 'name': '@CNAME',
                 'phone': /^1[385][1-9]\d{8}/,    //18600018456
@@ -56,11 +62,25 @@ table
     margin: 0 auto;
     margin-top: 100px;
     margin-bottom: 100px;
+    border-collapse: collapse;
 }
 .tag, .tag-content
 {
     border-left: 1px solid #c3c3c3;
     border-top: 1px solid #c3c3c3;
+}
+.tag:last-of-type, .table-tr:last-of-type, .tag-content:last-of-type
+{
+    border-bottom: 1px solid #c3c3c3;
+    border-right: 1px solid #c3c3c3;
+}
+.table-tr:nth-last-of-type(2n+1)
+{
+    background-color: #ededed;
+}
+.table-tr:hover
+{
+    background-color: #b6f5b6;
 }
 .tag
 {
@@ -70,5 +90,38 @@ table
 .tag-content
 {
     padding: 10px;
+}
+.table-button
+{
+    padding: 7px 15px;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    cursor: pointer;
+}
+.table-button:last-of-type
+{
+    margin-left: 10px;
+}
+#edit
+{
+    background-color: #fff;
+}
+#edit:hover
+{
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff
+}
+#delete
+{
+    color: #fff;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+}
+#delete:hover
+{
+    background-color: #f78989;
+    border-color: #f78989;
+    color: #fff;
 }
 </style>
